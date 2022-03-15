b47ec93d9a8d5a165f49f7135516d62fd35ccf78
Dukler <iarwain@hotmail.com.ar>
2022-03-15 07:20:21 -0300

    mobile fix

--- src/components/useIsMobile.js
+++ src/components/useIsMobile.js
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 
 const useIsMobile = () => {
-    const isBrowser = () => typeof window !== "undefined" && window.innerWidth
-    const [width, setWidth] = useState(isBrowser() ? window.innerWidth : 0);
+    const [width, setWidth] = useState(0);
     
     const handleWindowSizeChange = () => {
-            setWidth(window.innerWidth);
+        setWidth(window.innerWidth);
     }
-
+    useLayoutEffect(() => {
+        handleWindowSizeChange();
+    }, []);
+    
     useEffect(() => {
         window.addEventListener('resize', handleWindowSizeChange);
         return () => {
